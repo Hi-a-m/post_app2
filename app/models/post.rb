031f74f6ed72bbf41091e8b72dcd198a4ce8dcb3
Hi-a-m <riverdale.kampw@gmail.com>
2023-01-09 22:09:10 +0900

    Rails課題1再提出(2回目)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,16 @@
 class Post < ApplicationRecord
-    validates :title, presence: true
+    validates :title, presence: true, length: { maximum: 20 }
     validates :start_at, presence: true
     validates :end_at, presence: true
     validates :memo, length: { maximum: 500 }
     validate :start_end_check
-      def start_end_check
-        errors.add(:end_at, "の日付を正しく記入してください。") unless
-        self.start_at <= self.end_at
+
+    def start_end_check
+      if start_at.present? && end_at.present?
+        if end_at < start_at
+          errors.add(:end_at,"は開始日より後の日付を入力してください")
+        end
       end
-    
+    end
 
 end
